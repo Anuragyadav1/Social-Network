a246c713c194e64bc729ac436ab65b30f7200cd0
Anurag Yadav <10anuragyadav2002@gmail.com>
2025-03-24 23:50:21 +0530

    Added unfriend functionality to friend list in home page

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,2 @@
 export const BASE_URL = "https://social-network-backend-qloz.onrender.com/api"; // Change this to your backend URL when deployed
+//export const BASE_URL = "http://localhost:5000/api"; // Change this to your backend URL when deployed
